30417b723308df1736854098e1907241d69f24a3
walking_men <sunhongfa@xingyun.com>
2016-05-13 15:48:32 +0800

    sun do it

--- app/src/main/java/com/just/sun/activitys/SunDanmuActivity.java
+++ app/src/main/java/com/just/sun/activitys/SunDanmuActivity.java
@@ -23,6 +23,7 @@ public class SunDanmuActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_danmu_sun);
         getWindow().setFlags(
